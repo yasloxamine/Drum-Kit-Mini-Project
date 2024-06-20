731ff5b877633f5f086612bd774e925d4a3fa58f
yasloxamine <89788362+yasloxamine@users.noreply.github.com>
2024-06-20 22:56:06 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 var numberOfDrums = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrums; i++) {
-  document.querySelectorAll("button")[i].addEventListener("click", HandleClick);
+  document.querySelectorAll("button")[i].addEventListener("click", function(){
+    var buttonInnerHTML = this.innerHTML;
+   
+MakeSound(buttonInnerHTML);
+ButtonAnimation(buttonInnerHTML);
+  });
+
+  
 }
 
-function HandleClick() {
-  var buttonClicked = this.innerHTML;
+document.addEventListener("keydown",function(event){
+  MakeSound(event.key);
 
-  switch (buttonClicked) {
+  ButtonAnimation(event.key);
+});
+
+function MakeSound(key) {
+
+  switch (key) {
     case "w":
       var tom1 = new Audio("./sounds/tom-1.mp3");
       tom1.play();
       break;
 
     case "a":
-      var tom2= new Audio("./sounds/tom-2.mp3");
+      var tom2 = new Audio("./sounds/tom-2.mp3");
       tom2.play();
       break;
 
@@ -23,28 +35,39 @@ function HandleClick() {
       tom3.play();
       break;
 
-      case "d":
+    case "d":
       var tom4 = new Audio("./sounds/tom-4.mp3");
       tom4.play();
       break;
 
-      case "j":
-        var crash = new Audio("./sounds/crash.mp3");
-        crash.play();
-        break;
+    case "j":
+      var crash = new Audio("./sounds/crash.mp3");
+      crash.play();
+      break;
 
-        case "k":
-        var kickBass = new Audio("./sounds/kick-bass.mp3");
-        kickBass.play();
-        break;
+    case "k":
+      var kickBass = new Audio("./sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
 
-        case "l":
-        var snare = new Audio("./sounds/snare.mp3");
-        snare.play();
-        break;
+    case "l":
+      var snare = new Audio("./sounds/snare.mp3");
+      snare.play();
+      break;
 
     default:
-        console.log(buttonClicked);
-      
+      console.log(key);
   }
 }
+
+function ButtonAnimation(currentKey){
+var activeKey = document.querySelector("."+currentKey);
+activeKey.classList.add("pressed");
+
+setTimeout(function(){
+activeKey.classList.remove("pressed")
+},100);
+
+}
+
+
